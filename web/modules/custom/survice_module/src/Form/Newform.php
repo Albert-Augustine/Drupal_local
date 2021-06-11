e06973dd94d4c714228dc203a548940099ead36f
Albert Augustine <albertaugustine1477@gmail.com>
2021-06-11 19:17:32 +0530

    Adding files

--- web/modules/custom/survice_module/src/Form/Newform.php
+++ web/modules/custom/survice_module/src/Form/Newform.php
@@ -12,6 +12,18 @@ class Newform extends FormBase {
     return 'codimth_simple_form_api';
   }
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['type_options'] = array(
+    '#type' => 'value',
+    '#value' => array('None' => t('None'),
+                      'Create' => t('Create'),
+                      'Update' => t('Update'))
+    );
+    $form['type'] = array(
+      '#title' => t('Project Type'),
+      '#type' => 'select',
+      '#description' => "Select the project count type.",
+      '#options' => $form['type_options']['#value'],
+    );
     $form['name'] = array(
       '#type' => 'textfield',
       '#title' => t('name:'),
@@ -49,19 +61,30 @@ class Newform extends FormBase {
     $config = $this->config('service_module.settings');
     $username = $config->get('username');
     $password = $config->get('password');
-    ddl($username);
+    //ddl($username);
     $name = $form_state->getValue('name');
     $email = $form_state->getValue('email');
     $url = $form_state->getValue('url');
+    $type = $form_state->getValue('type');
     $issuer  = ['name' => $name, 'email' => $email, 'url' => $url];
+    $id = '3I2tDhQLQzCX4_LhMQ5V6A';
 
-
-    $service = \Drupal::service('service_module.say_hello');
-    $service->badgr_initiate($username, $password);
-    $token = $service->badgr_initiate($username, $password);
-    $access_token = $token['accesstoken']; 
-    // ddl($access_token);
-    $service->badgr_create_issuer($access_token,$issuer);
+    if ($type == 'Create') {
+      $service = \Drupal::service('service_module.say_hello');
+      $service->badgr_initiate($username, $password);
+      $token = $service->badgr_initiate($username, $password);
+      $access_token = $token['accesstoken'];
+      //ddl($access_token); 
+      $service->badgr_create_issuer($access_token,$issuer);
+    }
+    elseif ($type == 'Update') {
+      $service = \Drupal::service('service_module.say_hello');
+      $service->badgr_initiate($username, $password);
+      $token = $service->badgr_initiate($username, $password);
+      $access_token = $token['accesstoken']; 
+      // ddl($access_token);
+      $service->badgr_update_issuer($access_token , $issuer, $id);
+    }
   }
   // $service = \Drupal::service('service_module.say_hello');
   //   dsm($service->badgr_initiate($username,$password));
